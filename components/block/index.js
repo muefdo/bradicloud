23c8a1e82549ae7f38618f82aaa5332fc248f917
muefdo <mefe.dogan@tedu.edu.tr>
2023-04-22 00:26:01 +0300

    bug fix

--- components/block/index.js
+++ components/block/index.js
@@ -8,8 +8,8 @@ const Block = () => {
     <div className={styles.Container}>
       <div className={styles.textContainer}>
         <h1 className={styles.mainHeader}>Tüm<br/> Projelerimiz ve<br/> Markalarımız</h1>
-        <p className={styles.description} >Bradi.tech olarak yenilikçi <br/>proje ve markalarımızla web'i <br/>geliştirmeye devam ediyoruz.</p>
-        <p className={styles.description}>Bu projelerimizi ve kaynak kodlarını<br/>bradi.cloud'da sizler için paylaşıyoruz.</p>
+        <p className={styles.description} >Bradi.tech olarak yenilikçi <br/>proje ve markalarımızla web&apos;i <br/>geliştirmeye devam ediyoruz.</p>
+        <p className={styles.description}>Bu projelerimizi ve kaynak kodlarını<br/>bradi.cloud&apos;da sizler için paylaşıyoruz.</p>
       </div>
       <div className={styles.imageFirst}> <Image src={bradicloudpng} alt='foto' width={400} height={400}/> </div>
     </div>
